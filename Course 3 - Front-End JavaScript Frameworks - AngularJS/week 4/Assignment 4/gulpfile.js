6bddd310f1d027af1f128c2b797d565e8d3189f1
Alberto Arias <alberto@boodland.com>
2017-06-07 17:05:46 +0200

    Copy over week 4/conFusion folder content within Assignment 4 folder

---
+++ Course 3 - Front-End JavaScript Frameworks - AngularJS/week 4/Assignment 4/gulpfile.js
@@ -0,0 +1,103 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
+    sequence = require('gulp-sequence'),
+    shell = require('gulp-shell'),
+    del = require('del');
+
+gulp.task('jshint', function() {
+    return gulp.src('app/scripts/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist', 'json-server/public']);
+});
+
+// Default task
+gulp.task('default',
+  sequence('clean', ['usemin', 'imagemin', 'copyfonts'])
+);
+
+gulp.task('usemin', ['jshint'], function () {
+  return gulp.src('./app/**/*.html')
+    .pipe(usemin({
+      css: [minifycss(), rev()],
+      js: [ngannotate(), uglify(), rev()]
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+  return del(['dist/images']), gulp.src('app/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('copyfonts', function() {
+   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+   gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**')
+    .pipe(gulp.dest('./json-server/public'));
+});
+
+gulp.task('startServer', shell.task([
+  'cd ./json-server/ & json-server --watch db.json'
+]));
+
+gulp.task('launch', shell.task([
+  'start chrome http://localhost:3000'
+]));
+
+gulp.task('publish',
+  sequence('default', 'deploy', ['startServer', 'launch'])
+);
+
+// Watch
+gulp.task('watch', ['browser-sync'], function() {
+  // Watch .js files
+  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+      // Watch image files
+  gulp.watch('app/images/**/*', ['imagemin']);
+
+});
+
+gulp.task('browser-sync', ['default'], function () { 
+   var files = [
+      'app/**/*.html',
+      'app/styles/**/*.css',
+      'app/images/**/*.png',
+      'app/scripts/**/*.js',
+      'dist/**/*'
+   ];
+
+   browserSync.init(files, {
+     server: {
+       baseDir: "dist",
+       index: "index.html"
+     }
+   });
+        // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', browserSync.reload);
+});
